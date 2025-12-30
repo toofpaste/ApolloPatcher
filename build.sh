eb3f197076c70b5e85c01051746fab12d9b435ed
Michael Larragueta <38309926+toofpaste@users.noreply.github.com>
2025-12-30 15:24:04 -0800

    Fix CI build by installing SDKs and handling roothide

--- build.sh
+++ build.sh
@@ -8,6 +8,10 @@ export -n PREFIX
 make clean
 make package THEOS_PACKAGE_SCHEME=rootless
 
-export -n PREFIX
-make clean
-make package THEOS_PACKAGE_SCHEME=roothide
+if [ -f "$THEOS/makefiles/package/roothide.mk" ]; then
+  export -n PREFIX
+  make clean
+  make package THEOS_PACKAGE_SCHEME=roothide
+else
+  echo "Skipping roothide package: scheme not available in current Theos checkout."
+fi
